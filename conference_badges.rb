911101e774a1920fac009c34d17fb444f105af2a
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-12-23 03:16:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,9 @@ name = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(name)
+  name.collect do |x|
+    badge_maker(name)
+  end
+end
