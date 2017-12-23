d58cfc5421ebafe4d04f07ef857bd06d885741bb
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-12-23 03:48:19 +0000

    updated conference_badges method

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+name = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  name.collect do |x|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(name)
+  name.collect.with.index do |i, index|
+    "Hello #{name}! you'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer
+  batch_badge_creator(name).each do |i|
+    puts i
+  end
+  assign_rooms(name).each do|i|
+    puts i
+end
